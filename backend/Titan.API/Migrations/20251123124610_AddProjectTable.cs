243417bf74e6dd6a1049c1da257b74f2281cfca0
Pavan <sara.pavansriram@gmail.com>
2025-11-23 18:19:11 +0530

    feat: add Project table migration

---
+++ backend/Titan.API/Migrations/20251123124610_AddProjectTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Titan.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
--- backend/Titan.API/Models/User.cs
+++ backend/Titan.API/Models/User.cs
@@ -3,9 +3,9 @@ namespace Titan.API.Models
     public class User
     {
         public int Id { get; set; }
-        public string Email { get; set; } = default;
+        public string Email { get; set; } = default!;
 
-        public string PasswordHash { get; set; }
+        public string PasswordHash { get; set; } = default!;
 
         public string? Username { get; set; }
 
